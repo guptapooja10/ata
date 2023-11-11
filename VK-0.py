3821012abacd4da31d4d14fe3760f78594380b4d
Niharika2208 <55751831+Niharika2208@users.noreply.github.com>
2023-11-11 20:02:29 +0100

    VK-0.py

--- VK-0.py
+++ VK-0.py
@@ -1,37 +1,10 @@
 import streamlit as st
 import pandas as pd
 import io
-from PIL import Image
-# from google.cloud import firestore
-# import os
+# from PIL import Image
 #
-# # Initialize Firestore client
-# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "anlagentechnik-aschersleben-firebase-adminsdk-sfoug-5eb13936b2.json"
-# db = firestore.Client()
-#
-#
-# # Function to get all collection names from Firestore database
-# def get_all_collections(db):
-#     excluded_collections = {'operators', 'posts', 'projects'}  # Set of collections to exclude
-#     collections = db.collections()
-#     return [collection.id for collection in collections if collection.id not in excluded_collections]
-#
-#
-# # Function to get all document IDs from a Firestore collection
-# def get_all_document_ids(collection_name):
-#     docs = db.collection(collection_name).stream()
-#     return [doc.id for doc in docs]
-#
-#
-# # Function to get data from Firestore for a specific document in a collection
-# def get_data_from_firestore(collection_name, document_id):
-#     doc_ref = db.collection(collection_name).document(document_id)
-#     doc = doc_ref.get()
-#     return doc.to_dict() if doc.exists else None
-#
-
-image = Image.open('logo_ata.png')
-st.image(image, caption='Ata Logo', use_column_width=True)
+# image = Image.open('logo_ata.png')
+# st.image(image, caption='Ata Logo', use_column_width=True)
 
 # Define data types and properties
 properties = {
@@ -49,15 +22,6 @@ units = {
     'Anzeichnen': 'min',
     'Schweißen': 'min',
 }
-# firestore_data = {}
-#
-# # Display a select box with all collection names
-# collection_names = get_all_collections(db)
-# selected_collection = st.selectbox('Select Collection:', options=collection_names)
-#
-# # Fetch and display the data for a known document ID ('Details') from the selected collection
-# if selected_collection:
-#     firestore_data = get_data_from_firestore(selected_collection, 'Details')
 
 field_mapping = {
     'Brennen': 'Brennen',
@@ -72,14 +36,6 @@ st.title("Vorkalkulation")
 # Initialize session state for each property
 if "data" not in st.session_state:
     st.session_state.data = {prop: "" for prop in properties}
-
-# # If firestore_data is fetched, update the session state
-# if firestore_data:
-#     for app_field, firestore_field in field_mapping.items():
-#         # Assuming 'Gegenstand' should map to 'Benennung' in Firestore
-#         if app_field == 'Gegenstand':
-#             firestore_field = 'Benennung'
-#         st.session_state.data[app_field] = firestore_data.get(firestore_field, "")
 
 col1, col2 = st.columns(2)
 
@@ -96,31 +52,16 @@ for prop in props_col2:
     # Use the session state data to populate the fields
     st.session_state.data[prop] = col2.text_input(prompt, value=st.session_state.data[prop]).strip()
 
-
 # Convert the user input data dictionary to a pandas DataFrame
-df = pd.DataFrame([st.session_state.data])
+df = pd.DataFrame(st.session_state.data, index=[0])  # Specify index to create a DataFrame with one row
 
-
-# Function to download DataFrame as Excel sheet
-def download_excel(df):
+if st.button("Download Excel"):
     output = io.BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    df.to_excel(writer, sheet_name='Sheet1', index=False)
-    writer.save()
-    return output.getvalue()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, sheet_name='Sheet1', index=False)
+    output.seek(0)
+    st.download_button("Download Excel File", output, key="download_excel", file_name="data.xlsx", mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
 
-
-# Function to download DataFrame as JSON
-def download_json(df):
-    return df.to_json(orient="records")
-
-
-# Provide download options
-if st.button("Download as Excel"):
-    excel_data = download_excel(df)
-    st.download_button("Download Excel File", excel_data, file_name="data.xlsx",
-                       mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-
-if st.button("Download as JSON"):
-    json_data = download_json(df)
+if st.button("Download JSON"):
+    json_data = df.to_json(orient="records")
     st.download_button("Download JSON File", json_data, file_name="data.json", mime="application/json")
