e630d2a059e14a81ac27eabcfe9e0602997f8cb6
guptapooja10 <poojaguptamech@gmail.com>
2023-11-13 21:49:57 +0100

    VK_ST_0.py

--- VK_ST_0.py
+++ VK_ST_0.py
@@ -3,11 +3,13 @@ import pandas as pd
 import io
 from PIL import Image
 from google.cloud import firestore
+from google.oauth2 import service_account
 import os
 
 # Initialize Firestore client
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "anlagentechnik-aschersleben-firebase-adminsdk-sfoug-5eb13936b2.json"
-db = firestore.Client()
+key_dict = st.secrets["textkey"]
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds)
 
 
 # Function to get all collection names from Firestore database
