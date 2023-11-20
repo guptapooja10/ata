3c2c4c1fbdcfd3cd23cea0c1a61182da4d802d6a
Niharika2208 <niharikajois@gmail.com>
2023-11-20 19:30:48 +0100

    Rolling over to previous code

--- VK-0.py
+++ VK-0.py
@@ -63,6 +63,8 @@ units = {
     'Schweißen': 'min',
 }
 
+firestore_data = {}
+
 # Display a select box with all collection names
 collection_names = get_all_collections(db)
 selected_collection = st.selectbox('Select Collection:', options=collection_names)
@@ -78,37 +80,21 @@ field_mapping = {
     'Ausführen Nr.': 'Ausführen Nr.'
 }
 
+
 st.title("Vorkalkulation")
 
 # Initialize session state for each property
-if "vk_0_data" not in st.session_state:
-    st.session_state.vk_0_data = {prop: "" for prop in properties}
+if "data" not in st.session_state:
+    st.session_state.data = {prop: "" for prop in properties}
 
-# Define a key in session state to track the currently selected collection
-if 'current_collection' not in st.session_state:
-    st.session_state.current_collection = None
+# If firestore_data is fetched, update the session state
+if firestore_data:
+    for app_field, firestore_field in field_mapping.items():
+        # Assuming 'Gegenstand' should map to 'Benennung' in Firestore
+        if app_field == 'Gegenstand':
+            firestore_field = 'Benennung'
+        st.session_state.data[app_field] = firestore_data.get(firestore_field, "")
 
-firestore_data = {}
-details_data = {}
-vk_0_data = {}
-
-# Check if the selected collection has changed
-if st.session_state.current_collection != selected_collection:
-    st.session_state.current_collection = selected_collection
-
-    # Clear the previous data from session state
-    st.session_state.vk_0_data = {prop: "" for prop in properties}
-
-    # Load new data from Firestore for the selected collection
-    if selected_collection:
-        firestore_data = get_data_from_firestore(selected_collection, 'Details')
-        vk_0_data = get_data_from_firestore(selected_collection, 'VK-0')
-
-# Update session state with data from 'VK-0'
-if vk_0_data:
-    for prop in properties:
-        if prop not in ['Kunde', 'Gegenstand', 'Zeichnungs- Nr.', 'Ausführen Nr.']:  # Remaining fields
-            st.session_state.vk_0_data[prop] = vk_0_data.get(prop, "")
 
 col1, col2 = st.columns(2)
 
@@ -118,15 +104,15 @@ props_col2 = list(properties.keys())[len(properties) // 2:]
 for prop in props_col1:
     prompt = f"{prop} ({units.get(prop, '')})"
     # Use the session state data to populate the fields
-    st.session_state.vk_0_data[prop] = col1.text_input(prompt, value=st.session_state.vk_0_data[prop]).strip()
+    st.session_state.data[prop] = col1.text_input(prompt, value=st.session_state.data[prop]).strip()
 
 for prop in props_col2:
     prompt = f"{prop} ({units.get(prop, '')})"
     # Use the session state data to populate the fields
-    st.session_state.vk_0_data[prop] = col2.text_input(prompt, value=st.session_state.vk_0_data[prop]).strip()
+    st.session_state.data[prop] = col2.text_input(prompt, value=st.session_state.data[prop]).strip()
 
 # Convert the user input data dictionary to a pandas DataFrame
-df = pd.DataFrame(st.session_state.vk_0_data, index=[0])  # Specify index to create a DataFrame with one row
+df = pd.DataFrame(st.session_state.data, index=[0])  # Specify index to create a DataFrame with one row
 
 # Transpose the DataFrame to have each column stacked vertically
 df_transposed = df.transpose()
@@ -144,8 +130,9 @@ if st.button("Download JSON"):
     json_data = df.to_json(orient="records")
     st.download_button("Download JSON File", json_data, file_name="data.json", mime="application/json")
 
+
 if st.button("Upload to Database"):
     # Convert session state data to the appropriate format for Firestore
     # Assuming your Firestore expects a dictionary with specific keys
-    upload_data = {field_mapping.get(k, k): v for k, v in st.session_state.vk_0_data.items()}
+    upload_data = {field_mapping.get(k, k): v for k, v in st.session_state.data.items()}
     upload_data_to_firestore(db, selected_collection, 'VK-0', upload_data)
