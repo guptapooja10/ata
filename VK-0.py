3faa8e9a26ab54d2f3b355c25a936b4a76d920c8
Niharika2208 <niharikajois@gmail.com>
2023-11-20 19:40:36 +0100

    Made changes - st.session_state.vk_0_data

--- VK-0.py
+++ VK-0.py
@@ -85,7 +85,7 @@ st.title("Vorkalkulation")
 
 # Initialize session state for each property
 if "data" not in st.session_state:
-    st.session_state.data = {prop: "" for prop in properties}
+    st.session_state.vk_0_data = {prop: "" for prop in properties}
 
 # If firestore_data is fetched, update the session state
 if firestore_data:
@@ -93,7 +93,7 @@ if firestore_data:
         # Assuming 'Gegenstand' should map to 'Benennung' in Firestore
         if app_field == 'Gegenstand':
             firestore_field = 'Benennung'
-        st.session_state.data[app_field] = firestore_data.get(firestore_field, "")
+        st.session_state.vk_0_data[app_field] = firestore_data.get(firestore_field, "")
 
 
 col1, col2 = st.columns(2)
@@ -104,15 +104,15 @@ props_col2 = list(properties.keys())[len(properties) // 2:]
 for prop in props_col1:
     prompt = f"{prop} ({units.get(prop, '')})"
     # Use the session state data to populate the fields
-    st.session_state.data[prop] = col1.text_input(prompt, value=st.session_state.data[prop]).strip()
+    st.session_state.vk_0_data[prop] = col1.text_input(prompt, value=st.session_state.vk_0_data[prop]).strip()
 
 for prop in props_col2:
     prompt = f"{prop} ({units.get(prop, '')})"
     # Use the session state data to populate the fields
-    st.session_state.data[prop] = col2.text_input(prompt, value=st.session_state.data[prop]).strip()
+    st.session_state.vk_0_data[prop] = col2.text_input(prompt, value=st.session_state.vk_0_data[prop]).strip()
 
 # Convert the user input data dictionary to a pandas DataFrame
-df = pd.DataFrame(st.session_state.data, index=[0])  # Specify index to create a DataFrame with one row
+df = pd.DataFrame(st.session_state.vk_0_data, index=[0])  # Specify index to create a DataFrame with one row
 
 # Transpose the DataFrame to have each column stacked vertically
 df_transposed = df.transpose()
@@ -134,5 +134,5 @@ if st.button("Download JSON"):
 if st.button("Upload to Database"):
     # Convert session state data to the appropriate format for Firestore
     # Assuming your Firestore expects a dictionary with specific keys
-    upload_data = {field_mapping.get(k, k): v for k, v in st.session_state.data.items()}
+    upload_data = {field_mapping.get(k, k): v for k, v in st.session_state.vk_0_data.items()}
     upload_data_to_firestore(db, selected_collection, 'VK-0', upload_data)
