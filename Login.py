f08f55ee0c27961c1277dd42e71eff5a200cca58
Niharika2208 <niharikajois@gmail.com>
2023-11-29 10:34:40 +0100

    Firebase-admin in requirements.txt

--- Login.py
+++ Login.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from google.cloud import firestore
+from google.oauth2 import service_account
 from PIL import Image
+import firebase_admin
+from firebase_admin import credentials, auth
 
 
 # Navigation bar
@@ -24,51 +27,44 @@ navigation_bar()
 image = Image.open('logo_ata.png')
 st.image(image, caption='Ata Logo', use_column_width=True)
 
-# Initialize Firestore client
-db = firestore.Client(project="anlagentechnik-aschersleben")
+# Load Firestore credentials from Streamlit secrets
+key_dict = st.secrets["textkey"]
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds)
+
+# Initialize Firebase Admin SDK
+firebase_cred = credentials.Certificate(key_dict)
+firebase_admin.initialize_app(firebase_cred)
 
 
-# Function to register a new user
 def register_user(email, password):
     try:
-        # Check if the user already exists
-        user_ref = db.collection('users').document(email)
-        if user_ref.get().exists:
-            st.error("User already exists. Please log in.")
-            return None
-
-        # Create a new user document in the 'users' collection
+        # Create user in Firebase Authentication
+        user = auth.create_user(email=email, password=password)
+        # Store user details in Firestore
+        user_ref = db.collection('users').document(user.uid)
         user_ref.set({
             'email': email,
-            'password': password
+            'uid': user.uid
         })
-        return user_ref
+        return user
     except Exception as e:
         st.error(f"Error registering user: {e}")
         return None
 
 
-# Function to authenticate a user
 def login_user(email, password):
     try:
-        # Retrieve the user document from the 'users' collection
-        user_ref = db.collection('users').document(email)
-        user_data = user_ref.get().to_dict()
-
-        # Check if the user exists and the password is correct
-        if user_data and user_data.get('password') == password:
-            return user_data
-        else:
-            st.error("Invalid email or password. Please try again.")
-            return None
+        # Authenticate user using Firebase Authentication
+        user = auth.sign_in_with_email_and_password(email, password)
+        return user
     except Exception as e:
         st.error(f"Authentication failed: {e}")
         return None
 
 
-# UI for login and registration
 def main():
-    st.title("Login Page")
+    st.title("Firestore Authentication Demo")
 
     page = st.sidebar.selectbox("Select Page", ["Login", "Register"])
 
@@ -78,12 +74,10 @@ def main():
         login_password = st.text_input("Password", type="password")
 
         if st.button("Login"):
-            user_data = login_user(login_email, login_password)
-            if user_data:
+            user = login_user(login_email, login_password)
+            if user:
                 st.success("Login successful!")
-                st.write(f"Welcome, {user_data['email']}")
-            else:
-                st.error("Login failed. Please check your credentials.")
+                st.write(f"Welcome, {user['email']} (UID: {user['localId']})")
 
     elif page == "Register":
         st.header("Register")
@@ -93,8 +87,8 @@ def main():
 
         if st.button("Register"):
             if register_password == confirm_password:
-                user_ref = register_user(register_email, register_password)
-                if user_ref:
+                user = register_user(register_email, register_password)
+                if user:
                     st.success("Registration successful! You can now log in.")
                 else:
                     st.error("Error during registration. Please try again.")
