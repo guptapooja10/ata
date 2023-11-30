7513b6a42e469e5aeb7f051097dc2c5e21f4334d
Niharika2208 <niharikajois@gmail.com>
2023-11-30 12:42:33 +0100

    Login page

--- Login.py
+++ Login.py
@@ -2,8 +2,6 @@ import streamlit as st
 from google.cloud import firestore
 from google.oauth2 import service_account
 from PIL import Image
-import firebase_admin
-from firebase_admin import credentials, auth
 
 
 # Navigation bar
@@ -27,74 +25,31 @@ navigation_bar()
 image = Image.open('logo_ata.png')
 st.image(image, caption='Ata Logo', use_column_width=True)
 
-# Load Firestore credentials from Streamlit secrets
+# Initialize Firestore client
 key_dict = st.secrets["textkey"]
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds)
 
-# Initialize Firebase Admin SDK
-firebase_cred = credentials.Certificate(key_dict)
-firebase_admin.initialize_app(firebase_cred)
+
+# Registering new users
+def register_users(user_id, name, email, password):
+    user_ref = db.collection('Users').document(user_id)
+    user_ref.set({
+        'name': name,
+        'email': email,
+        'password': password
+    })
 
 
-def register_user(email, password):
-    try:
-        # Create user in Firebase Authentication
-        user = auth.create_user(email=email, password=password)
-        # Store user details in Firestore
-        user_ref = db.collection('users').document(user.uid)
-        user_ref.set({
-            'email': email,
-            'uid': user.uid
-        })
-        return user
-    except Exception as e:
-        st.error(f"Error registering user: {e}")
-        return None
+action = st.radio('Select Action:', ['Register', 'Login'])
 
+if action == 'Register':
+    st.subheader('Register')
+    user_id = st.text_input('User ID:')
+    name = st.text_input('Name:')
+    email = st.text_input('E-Mail:')
+    password = st.text_input('Password:', type = 'password')
 
-def login_user(email, password):
-    try:
-        # Authenticate user using Firebase Authentication
-        user = auth.sign_in_with_email_and_password(email, password)
-        return user
-    except Exception as e:
-        st.error(f"Authentication failed: {e}")
-        return None
-
-
-def main():
-    st.title("Firestore Authentication Demo")
-
-    page = st.sidebar.selectbox("Select Page", ["Login", "Register"])
-
-    if page == "Login":
-        st.header("Login")
-        login_email = st.text_input("Email")
-        login_password = st.text_input("Password", type="password")
-
-        if st.button("Login"):
-            user = login_user(login_email, login_password)
-            if user:
-                st.success("Login successful!")
-                st.write(f"Welcome, {user['email']} (UID: {user['localId']})")
-
-    elif page == "Register":
-        st.header("Register")
-        register_email = st.text_input("Email")
-        register_password = st.text_input("Password", type="password")
-        confirm_password = st.text_input("Confirm Password", type="password")
-
-        if st.button("Register"):
-            if register_password == confirm_password:
-                user = register_user(register_email, register_password)
-                if user:
-                    st.success("Registration successful! You can now log in.")
-                else:
-                    st.error("Error during registration. Please try again.")
-            else:
-                st.error("Passwords do not match. Please try again.")
-
-
-if __name__ == "__main__":
-    main()
+    if st.button('Register'):
+        register_users(user_id, name, email, password)
+        st.success("Registration successful!")
