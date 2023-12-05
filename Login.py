0ae5db09c3d3a12d9d0d8f6ef5cd47053c191788
Niharika2208 <niharikajois@gmail.com>
2023-12-05 18:25:10 +0100

    Changed the code

--- Login.py
+++ Login.py
@@ -1,36 +1,71 @@
 import streamlit as st
 import firebase_admin
-from firebase_admin import credentials, auth
+from firebase_admin import firestore
+from firebase_admin import credentials
+from firebase_admin import auth
 
-cred = credentials.Certificate('anlagentechnik-aschersleben-fd030234653c.json')
+cred = credentials.Certificate("anlagentechnik-aschersleben-fd030234653c.json")
 firebase_admin.initialize_app(cred)
 
-# Check if Firebase app is not already initialized
-# Initialize Firebase app if not already initialized
-try:
-    cred = credentials.Certificate('anlagentechnik-aschersleben-fd030234653c.json')
-    firebase_admin.initialize_app(cred)
-except ValueError:
-    pass  # App is already initialized
+
+def app():
+    # Usernm = []
+    st.title('Welcome to :violet[Pondering] :sunglasses:')
+
+    if 'username' not in st.session_state:
+        st.session_state.username = ''
+    if 'useremail' not in st.session_state:
+        st.session_state.useremail = ''
+
+    def f():
+        try:
+            user = auth.get_user_by_email(email)
+            print(user.uid)
+            st.session_state.username = user.uid
+            st.session_state.useremail = user.email
+
+            global Usernm
+            Usernm = (user.uid)
+
+            st.session_state.signedout = True
+            st.session_state.signout = True
 
 
-def login_app():
-    st.title('Welcome to :violet[ATA]')
+        except:
+            st.warning('Login Failed')
 
-    choice = st.selectbox('Login/Signup', ['Login', 'Signup'])
-    if choice == 'Login':
-        email = st.text_input('E-Mail Address')
+    def t():
+        st.session_state.signout = False
+        st.session_state.signedout = False
+        st.session_state.username = ''
+
+    if "signedout" not in st.session_state:
+        st.session_state["signedout"] = False
+    if 'signout' not in st.session_state:
+        st.session_state['signout'] = False
+
+    if not st.session_state["signedout"]:  # only show if the state is False, hence the button has never been clicked
+        choice = st.selectbox('Login/Signup', ['Login', 'Sign up'])
+        email = st.text_input('Email Address')
         password = st.text_input('Password', type='password')
 
-        st.button('Login')
+        if choice == 'Sign up':
+            username = st.text_input("Enter  your unique username")
 
-    else:
-        email = st.text_input('E-Mail Address')
-        password = st.text_input('Password', type='password')
-        username = st.text_input('Enter your username')
+            if st.button('Create my account'):
+                user = auth.create_user(email=email, password=password, uid=username)
 
-        if st.button('Create my account'):
-            user = auth.create_user(email=email, password=password, uid=username)
-            st.success('Account created successfully!')
-            st.markdown('You can now login using your E-Mail and Password')
-            st.balloons()
+                st.success('Account created successfully!')
+                st.markdown('Please Login using your email and password')
+                st.balloons()
+        else:
+            # st.button('Login', on_click=f)
+            st.button('Login', on_click=f)
+
+    if st.session_state.signout:
+        st.text('Name ' + st.session_state.username)
+        st.text('Email id: ' + st.session_state.useremail)
+        st.button('Sign out', on_click=t)
+
+    def ap():
+        st.write('Posts')
