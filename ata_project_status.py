50c6a12513d3d0744c672d01cc974af5346edf89
Niharika2208 <niharikajois@gmail.com>
2023-12-11 18:40:45 +0100

    changes to the status app

--- ata_project_status.py
+++ ata_project_status.py
@@ -102,10 +102,12 @@ def main():
     for doc_id in document_ids:
         total_fields = get_total_fields(selected_collection, doc_id)
         st.write(f"{doc_id}: {total_fields} fields")
-        st.write(f"Total fields in {doc_id}: {total_fields}")
+        # Display total fields for debugging
+        st.text(f"Total fields in {doc_id}: {total_fields}")
         populated_fields_count = get_populated_fields_count(selected_collection, doc_id)
         st.write(f"{doc_id}: {populated_fields_count} populated fields")
-        st.write(f"Populated fields count in {doc_id}: {populated_fields_count}")
+        # Display populated fields count for debugging
+        st.text(f"Populated fields count in {doc_id}: {populated_fields_count}")
 
 
 if __name__ == '__main__':
