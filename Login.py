858c85cadc4567537226dffc8034365b45614f90
Niharika2208 <niharikajois@gmail.com>
2024-02-10 12:54:16 +0100

    Multipage app

--- Login.py
+++ Login.py
@@ -1,8 +1,6 @@
 import streamlit as st
 from firebase_init import initialize_firebase_app
 from firebase_admin import auth
-import os
-import tempfile
 
 # Initialize Firebase app if not already initialized
 initialize_firebase_app()
@@ -11,8 +9,6 @@ initialize_firebase_app()
 def get_session_state():
     if 'authenticated' not in st.session_state:
         st.session_state.authenticated = False
-    if 'page' not in st.session_state:
-        st.session_state.page = 'login'
 
 
 # Call get_session_state before any Streamlit function
@@ -22,15 +18,14 @@ get_session_state()
 def login_app():
     st.title('Welcome to :violet[ATA]')
 
-    if st.session_state.page == 'login':
+    choice = st.selectbox('Login/Signup', ['Login', 'Signup'])
+    if choice == 'Login':
         email = st.text_input('E-Mail Address')
         password = st.text_input('Password', type='password')
 
         if st.button('Login'):
             # Assuming authentication is successful, set st.session_state.authenticated to True
             st.session_state.authenticated = True
-            # Change the page to 'file_upload'
-            st.session_state.page = 'file_upload'
 
     else:
         email = st.text_input('E-Mail Address')
@@ -44,25 +39,6 @@ def login_app():
             st.balloons()
 
 
-def file_upload_page():
-    st.header('File Upload Page')
-    st.write("Upload your important documents here:")
-
-    uploaded_files = st.file_uploader("Choose a file", type=["pdf", "txt", "csv", "xlsx"])
-
-    if uploaded_files is not None:
-        st.success("File uploaded successfully!")
-
-        # Process the uploaded file as needed
-        # For example, you can save it to a temporary directory
-        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-            temp_file.write(uploaded_files.read())
-            st.write("Temporary file saved at:", temp_file.name)
-
-        # Clean up the temporary file
-        os.remove(temp_file.name)
-
-
 if __name__ == "__main__":
     # Call get_session_state before any Streamlit function
     get_session_state()
@@ -72,6 +48,12 @@ if __name__ == "__main__":
 
     # Handle navigation or display other pages based on authentication status
     if st.session_state.authenticated:
-        # Display file upload functionality based on the current page state
-        if st.session_state.page == 'file_upload':
-            file_upload_page()
+        st.title("Simple File Upload App")
+
+        # File upload widget
+        uploaded_file = st.file_uploader("Choose a file")
+
+        # Check if a file is uploaded
+        if uploaded_file is not None:
+            # Display a simple confirmation message
+            st.success(f"File uploaded successfully!")
