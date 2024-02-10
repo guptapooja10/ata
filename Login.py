8764be6a56edcd58da5c1f5d559a4905ba6e5b1c
Niharika2208 <niharikajois@gmail.com>
2024-02-10 16:26:57 +0100

    Added st.link_button

--- Login.py
+++ Login.py
@@ -24,14 +24,14 @@ def login_app():
         password = st.text_input('Password', type='password')
 
         # Check if the "Login" button is clicked
-        if st.button('Login'):
+        if st.checkbox('Admin'):
             # Assuming authentication is successful, set st.session_state.authenticated to True
             st.session_state.authenticated = True
 
             # Redirect to the specified URL if authenticated
             if st.session_state.authenticated:
                 st.experimental_set_query_params(app='project_instantiation')
-                st.link_button("Go to App", "https://ata-app-navigator.streamlit.app/")
+                st.link_button("Login", "https://ata-app-navigator.streamlit.app/")
                 st.link_button()
 
 
