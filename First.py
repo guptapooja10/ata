39dc0fe383a3cb80c6655578b8880409567fb64f
Niharika2208 <niharikajois@gmail.com>
2024-02-14 11:16:14 +0100

    Image carousel

--- First.py
+++ First.py
@@ -1,13 +1,15 @@
 import streamlit as st
 import time
 
-st.title("About")
 image_list = ["1.jpg", "2.jpg", "3.jpg"]
 
 interval = 2
 
-st.title("ATA")
+col1, col2, col3 = st.beta_columns(3)
+
+# Loop through the images
 for i in range(len(image_list)):
-    st.image(image_list[i], caption=f"Image {i + 1}", use_column_width=True)
+    with col1:
+        st.image(image_list[i], caption=f"Image {i + 1}", use_column_width=True)
 
     time.sleep(interval)
