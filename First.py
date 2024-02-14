e0579bd7337da19c030612e3433f65c7b8d9911a
Niharika2208 <niharikajois@gmail.com>
2024-02-14 11:19:21 +0100

    Image carousel

--- First.py
+++ First.py
@@ -5,11 +5,10 @@ image_list = ["1.jpg", "2.jpg", "3.jpg"]
 
 interval = 2
 
-col1, col2, col3 = st.columns(3)
+container = st.container()
 
 # Loop through the images
 for i in range(len(image_list)):
-    with col1:
-        st.image(image_list[i], caption=f"Image {i + 1}", use_column_width=True)
+    container.image(image_list[i], caption=f"Image {i + 1}", use_column_width=True)
 
     time.sleep(interval)
