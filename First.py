cd8e02df7e7985e6cb79094be483a9dbfdc575da
Niharika2208 <niharikajois@gmail.com>
2024-02-14 11:17:31 +0100

    Image carousel

--- First.py
+++ First.py
@@ -5,7 +5,7 @@ image_list = ["1.jpg", "2.jpg", "3.jpg"]
 
 interval = 2
 
-col1, col2, col3 = st.beta_columns(3)
+col1, col2, col3 = st.columns(3)
 
 # Loop through the images
 for i in range(len(image_list)):
