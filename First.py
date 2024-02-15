99b426d49ce1433ff3bd7232a76ffb21c2629f24
Niharika2208 <niharikajois@gmail.com>
2024-02-15 10:43:18 +0100

    testing

--- First.py
+++ First.py
@@ -1,41 +1,25 @@
 import streamlit as st
-# from matplotlib import pyplot as plt
 import numpy as np
+from PIL import Image
 
 
 @st.experimental_memo
-def get_numbers():
-    sets = []
+def get_images():
+    image_paths = [
+        "1.jpg",
+        "2.jpg",
+        "3.jpg",
+    ]
+
+    images = [Image.open(image_path) for image_path in image_paths]
+    return images
+
+
+with st.expander('Using tabs'):
+    tabs = st.tabs(list(np.array(range(1, 11)).astype(str)))
+
+    images = get_images()
+
     for i in range(10):
-        sets.append(np.random.normal(1, 1, size=100))
-    return sets
-
-
-sets = get_numbers()
-
-# @st.experimental_memo
-# def get_plot(i):
-#   fig, ax = plt.subplots()
-#  ax.hist(sets[i], bins=20)
-# return fig
-
-
-# create a bunch of figures
-# figs = [get_plot(i) for i in range(10)]
-
-# with st.expander('Using a slider'):
-
-#   plot = st.container()
-
-# use st.slider to select
-#  index = st.slider('figure index', 1, 10)
-
-# use st.pyplot
-# with plot:
-#    st.pyplot(figs[index-1])
-
-# with st.expander('Using tabs'):
-#    tabs = st.tabs(list(np.array(range(1, 11)).astype(str)))
-
-#   for i in range(10):
-#      tabs[i].pyplot(figs[i])
+        # Display an image in each tab
+        tabs[i].image(images[i], caption=f"Image {i + 1}")
