f5e957608727884bb89d3781ef669ee27e05ecb4
Niharika2208 <niharikajois@gmail.com>
2024-02-15 18:28:30 +0100

    Login page changes

--- Login.py
+++ Login.py
@@ -28,18 +28,19 @@ def login_app():
 
         if st.checkbox('Admin'):
             st.session_state.authenticated = True
+            if is_admin_checked and is_not_admin_checked:
+                st.warning("You can't choose both the options")
             if st.session_state.authenticated:
-                if is_admin_checked and is_not_admin_checked:
-                    st.experimental_set_query_params(app='project_instantiation')
-                    st.link_button("Sign In", "https://ata-app-navigator.streamlit.app/")
-
+                st.experimental_set_query_params(app='project_instantiation')
+                st.link_button("Sign In", "https://ata-app-navigator.streamlit.app/")
 
         if st.checkbox('Not an Admin'):
             st.session_state.authenticated = True
+            if is_admin_checked and is_not_admin_checked:
+                st.warning("You can't choose both the options")
             if st.session_state.authenticated:
-                if is_admin_checked and is_not_admin_checked:
-                    st.experimental_set_query_params(app='Project_Status_App')
-                    st.link_button("Sign In", "https://ata-project-status.streamlit.app/")
+                st.experimental_set_query_params(app='Project_Status_App')
+                st.link_button("Sign In", "https://ata-project-status.streamlit.app/")
 
 
     else:
