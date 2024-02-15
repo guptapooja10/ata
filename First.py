82425cd1d71df72be3556695e29351b27592fa1b
Niharika2208 <niharikajois@gmail.com>
2024-02-15 11:00:49 +0100

    testing

--- First.py
+++ First.py
@@ -2,7 +2,6 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 
-
 @st.cache_data
 def get_images():
     image_paths = [
@@ -14,12 +13,10 @@ def get_images():
     images = [Image.open(image_path) for image_path in image_paths]
     return images
 
+tabs = st.tabs(list(np.array(range(1, 4)).astype(str)))
 
-#with st.expander('Using tabs'):
-    tabs = st.tabs(list(np.array(range(1, 11)).astype(str)))
+images = get_images()
 
-    images = get_images()
-
-    for i in range(3):
-        # Display an image in each tab
-        tabs[i].image(images[i], caption=f"Image {i + 1}")
+for i in range(len(images)):
+    # Display an image in each tab
+    tabs[i].image(images[i], caption=f"Image {i + 1}")
