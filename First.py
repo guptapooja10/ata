2225bcc15c05daeb1537468655ed8cda96f791d2
Niharika2208 <niharikajois@gmail.com>
2024-02-15 12:04:27 +0100

    About page

--- First.py
+++ First.py
@@ -23,7 +23,7 @@ def about_page():
         images = [Image.open(image_path) for image_path in image_paths]
         return images
 
-    tabs = st.tabs(list(np.array(range(1, 4)).astype(str)))
+    tabs = st.tabs(list(np.array(range(1, 5)).astype(str)))
 
     images = get_images()
 
