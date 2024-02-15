41a2b74c7b071b281e7ab26f7ce79b50a95b61b4
Niharika2208 <niharikajois@gmail.com>
2024-02-15 18:52:00 +0100

    Login page changes

--- Login.py
+++ Login.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from firebase_init import initialize_firebase_app
-from firebase_admin import auth, exceptions
+from firebase_admin import auth
 
 # Initialize Firebase app if not already initialized
-firebase_app = initialize_firebase_app()  # Assuming your initialize_firebase_app() function returns the app instance
+initialize_firebase_app()
 
 
 def get_session_state():
@@ -30,18 +30,18 @@ def login_app():
             st.warning("Please select only one option (Admin or Not an Admin)")
         else:
             if email and password:  # Check if email and password are provided
-                try:
-                    # Sign in with email and password
-                    user = auth.sign_in_with_email_and_password(email, password, app=firebase_app)
+                if is_admin_checked:
+                    # Perform authentication only if email and password are provided
                     st.session_state.authenticated = True
-                    if is_admin_checked:
+                    if st.session_state.authenticated:
                         st.experimental_set_query_params(app='project_instantiation')
                         st.link_button("Sign In", "https://ata-app-navigator.streamlit.app/")
-                    elif is_not_admin_checked:
+                elif is_not_admin_checked:
+                    # Perform authentication only if email and password are provided
+                    st.session_state.authenticated = True
+                    if st.session_state.authenticated:
                         st.experimental_set_query_params(app='Project_Status_App')
                         st.link_button("Sign In", "https://ata-project-status.streamlit.app/")
-                except exceptions.FirebaseError as e:
-                    st.warning("Invalid email or password. Please try again.")
             elif is_admin_checked or is_not_admin_checked:
                 st.warning("Please enter both email and password before attempting to sign in.")
     else:
@@ -50,14 +50,10 @@ def login_app():
         username = st.text_input('Enter your username')
 
         if st.button('Create my account'):
-            try:
-                # Create a new user with email and password
-                user = auth.create_user_with_email_and_password(email, password, app=firebase_app)
-                st.success('Account created successfully!')
-                st.markdown('You can now log in using your E-Mail and Password')
-                st.balloons()
-            except exceptions.FirebaseError as e:
-                st.warning(f"Account creation failed: {e.message}")
+            user = auth.create_user(email=email, password=password, uid=username)
+            st.success('Account created successfully!')
+            st.markdown('You can now log in using your E-Mail and Password')
+            st.balloons()
 
 
 if __name__ == "__main__":
