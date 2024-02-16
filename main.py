24d291acedf7fb934a28abeea0c6f645a7310488
Niharika2208 <niharikajois@gmail.com>
2024-02-16 10:57:52 +0100

    Commented out navigation in main

--- main.py
+++ main.py
@@ -99,18 +99,18 @@ def main():
     image = Image.open('logo_ata.png')
     st.image(image, caption='Ata Logo')
     # Navigation bar
-    apps = {
-        "Login page": "https://credentials-page.streamlit.app/",
-        "VK-ST-0": "https://vk-st-0.streamlit.app/",
-        "VK-0": "https://ata-vk-0.streamlit.app/",
-        "Deckung": "https://deckung.streamlit.app/",
-        "ATA-Project-Status": "https://ata-project-status.streamlit.app/"
-    }
+    #apps = {
+     #   "Login page": "https://credentials-page.streamlit.app/",
+      #  "VK-ST-0": "https://vk-st-0.streamlit.app/",
+       # "VK-0": "https://ata-vk-0.streamlit.app/",
+        #"Deckung": "https://deckung.streamlit.app/",
+       # "ATA-Project-Status": "https://ata-project-status.streamlit.app/"
+   # }
 
-    st.sidebar.title('Navigation')
+    #st.sidebar.title('Navigation')
 
-    for app_name, app_url in apps.items():
-        st.sidebar.markdown(f"[{app_name}]({app_url})")
+    #for app_name, app_url in apps.items():
+     #   st.sidebar.markdown(f"[{app_name}]({app_url})")
 
     st.header('Project Instantiation')
     # Input fields for project instantiation
