7d24aa21bb460aa26ceff01940a63b7601c1914e
Niharika2208 <niharikajois@gmail.com>
2024-02-16 11:50:15 +0100

    sac menu

--- main.py
+++ main.py
@@ -103,6 +103,7 @@ def main():
     sac.segmented(
         items=[
             sac.SegmentedItem(label='VK-ST-0', icon='share-fill', href='https://vk-st-0.streamlit.app/'),
+            sac.SegmentedItem(label='VK-ST-0', icon='share-fill', href='https://vk-st-0.streamlit.app/'),
         ], align='left', direction='vertical'
     )
 
