1606b32a695701a257c60c311c363898b2454319
Niharika2208 <niharikajois@gmail.com>
2024-03-07 14:34:48 +0100

    Navigation changes

--- main.py
+++ main.py
@@ -118,6 +118,7 @@ def main():
         items=[
             sac.SegmentedItem(label='About', href='https://aboutpage.streamlit.app/'),
             sac.SegmentedItem(label='Sign In', href='https://credentials-page.streamlit.app/'),
+            sac.SegmentedItem(label='Project Instantiation', href='https://ata-app-navigator.streamlit.app/'),
             sac.SegmentedItem(label='Material List', href='https://vk-st-0.streamlit.app/'),
             sac.SegmentedItem(label='Deckung', href='https://deckung.streamlit.app/'),
             sac.SegmentedItem(label='Angebot', href='https://deckung.streamlit.app/'),
