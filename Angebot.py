b5790fd7b2a839254476929f8acb2e5479e5178d
Niharika2208 <niharikajois@gmail.com>
2024-03-07 15:01:18 +0100

    Navigation changes

--- Angebot.py
+++ Angebot.py
@@ -5,21 +5,21 @@ from google.cloud import firestore
 from google.oauth2 import service_account
 from datetime import datetime
 from io import BytesIO
-import streamlit_antd_components as sac
+#import streamlit_antd_components as sac
 
 
-sac.segmented(
+#sac.segmented(
 
-        items=[
-            sac.SegmentedItem(label='About', href='https://aboutpage.streamlit.app/'),
-            sac.SegmentedItem(label='Sign In', href='https://credentials-page.streamlit.app/'),
-            sac.SegmentedItem(label='Project Instantiation', href='https://ata-app-navigator.streamlit.app/'),
-            sac.SegmentedItem(label='Material List', href='https://vk-st-0.streamlit.app/'),
-            sac.SegmentedItem(label='Deckung', href='https://deckung.streamlit.app/'),
-            sac.SegmentedItem(label='Angebot', href='https://angebot.streamlit.app/'),
-            sac.SegmentedItem(label='Project Status', href='https://ata-project-status.streamlit.app/'), ],
-        align='end', size='sm', radius='sm'
-    )
+#        items=[
+#            sac.SegmentedItem(label='About', href='https://aboutpage.streamlit.app/'),
+#            sac.SegmentedItem(label='Sign In', href='https://credentials-page.streamlit.app/'),
+#            sac.SegmentedItem(label='Project Instantiation', href='https://ata-app-navigator.streamlit.app/'),
+#            sac.SegmentedItem(label='Material List', href='https://vk-st-0.streamlit.app/'),#
+#            sac.SegmentedItem(label='Deckung', href='https://deckung.streamlit.app/'),
+#            sac.SegmentedItem(label='Angebot', href='https://angebot.streamlit.app/'),
+#            sac.SegmentedItem(label='Project Status', href='https://ata-project-status.streamlit.app/'), ],
+#        align='end', size='sm', radius='sm'
+#    )
 
 
 # Initialize Firestore client
