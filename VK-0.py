c8d93148325dd54cdc2aa7bb541756e32d722647
Niharika2208 <niharikajois@gmail.com>
2024-03-17 18:35:06 +0100

    Navigation changes

--- VK-0.py
+++ VK-0.py
@@ -4,22 +4,22 @@ import io
 from PIL import Image
 from google.cloud import firestore
 from google.oauth2 import service_account
+import streamlit_antd_components as sac
 import os
 
-# Navigation bar
-# def navigation_bar():
-#   apps = {
-#        "Login page": "https://credentials-page.streamlit.app/",
-#        "Project Instantiation": "https://ata-app-navigator.streamlit.app/",
-#        "VK-ST-0": "https://vk-st-0.streamlit.app/",
-#        "Deckung": "https://deckung.streamlit.app/",
-#        "ATA-Project-Status": "https://ata-project-status.streamlit.app/"
-#    }
+sac.segmented(
 
-#    st.sidebar.title('Navigation')
-
-#    for app_name, app_url in apps.items():
-#        st.sidebar.markdown(f"[{app_name}]({app_url})")
+        items=[
+            sac.SegmentedItem(label='Schweißen', href='https://ata-vk-0.streamlit.app/'),
+            sac.SegmentedItem(label='About', href='https://aboutpage.streamlit.app/'),
+            sac.SegmentedItem(label='Sign In', href='https://credentials-page.streamlit.app/'),
+            sac.SegmentedItem(label='Project Instantiation', href='https://ata-app-navigator.streamlit.app/'),
+            sac.SegmentedItem(label='Material List', href='https://vk-st-0.streamlit.app/'),
+            sac.SegmentedItem(label='Deckung', href='https://deckung.streamlit.app/'),
+            sac.SegmentedItem(label='Angebot', href='https://angebot.streamlit.app/'),
+            sac.SegmentedItem(label='Project Status', href='https://ata-project-status.streamlit.app/'), ],
+        align='end', size='sm', bg_color='transparent'
+    )
 
 
 # navigation_bar()
